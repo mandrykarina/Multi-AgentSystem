22df65d161caeb0db9437312bf526449891ac91a
Arina <98313159+mandrykarina@users.noreply.github.com>
2026-05-17 18:29:23 +0300

    Delete agents directory

--- agents/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Агенты PMR."""
-
-from .agent1 import analyze_task
-from .agent2 import execute_task
-from .agent3 import reflect_solution
-
-__all__ = ["analyze_task", "execute_task", "reflect_solution"]
-
